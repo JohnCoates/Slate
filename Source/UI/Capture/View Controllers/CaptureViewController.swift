4101b184f928350b69f6f95a5bf23ef48cc8b356
John Coates <acapulco1988@gmail.com>
2016-09-26 04:24:40 -0700

    added capture view placeholder for simulator

--- Source/UI/Capture/View Controllers/CaptureViewController.swift
+++ Source/UI/Capture/View Controllers/CaptureViewController.swift
@@ -12,61 +12,72 @@ import Cartography
 import GPUImage
 
 final class CaptureViewController: UIViewController {
-    
+
     // MARK: - View Lifecycle
-    
+
     private var videoCamera: Camera?
     override func loadView() {
         super.loadView()
         view.accessibilityIdentifier = "CaptureView"
         view.backgroundColor = UIColor.black
-        
+
         renderViewSetup()
         controlsSetup()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if !Platform.isSimulator {
+
+        if Platform.isSimulator {
+            placeholderSetup()
+        } else {
             cameraSetup()
         }
     }
-    
+
     // MARK: - Render
-    
+
     private var renderView: RenderView!
     fileprivate func renderViewSetup() {
         renderView = RenderView(frame: view.bounds)
         renderView.fillMode = .preserveAspectRatioAndFill
         view.addSubview(renderView)
     }
-    
+
     // MARK: - Camera Setup
-    
+
     fileprivate func cameraSetup() {
         do {
             videoCamera = try Camera(sessionPreset: AVCaptureSessionPresetPhoto, location: .backFacing)
         } catch {
             print("Couldn't initialize camera, error: \(error)")
         }
-        
+
         guard let videoCamera = videoCamera else {
             return
         }
-        
+
         videoCamera.addTarget(renderView)
         videoCamera.startCapture()
     }
-    
+
+    // MARK: - Placeholder
+
+    fileprivate func placeholderSetup() {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "HannahDeathValley"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.frame = view.bounds
+        view.insertSubview(imageView, at: 0)
+    }
+
     // MARK: - Controls Setup
-    
+
     private lazy var captureButton: CaptureButton = CaptureButton()
     fileprivate func controlsSetup() {
         captureButton.setTappedCallback(instance: self,
                                         method: Method.captureTapped)
         view.addSubview(captureButton)
-        
+
         constrain(captureButton) {
             let superview = $0.superview!
             $0.width == 75
@@ -75,15 +86,15 @@ final class CaptureViewController: UIViewController {
             $0.bottom == superview.bottom - 15
         }
     }
-    
+
     // MARK: - Capturing
-    
+
     fileprivate func captureTapped() {
         print("capture tapped")
     }
-    
+
     // MARK: - Status Bar
-    
+
     override var prefersStatusBarHidden: Bool {
         get {
             return true
@@ -95,5 +106,5 @@ final class CaptureViewController: UIViewController {
 
 fileprivate struct Method {
     static let captureTapped = CaptureViewController.captureTapped
-    
+
 }
