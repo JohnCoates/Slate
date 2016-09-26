b0131cb39cddc720b64d6f0cfb812ca61231afd8
John Coates <acapulco1988@gmail.com>
2016-09-26 01:52:35 -0700

    set up GPUImage to render to screen

--- Source/UI/Capture/View Controllers/CaptureViewController.swift
+++ Source/UI/Capture/View Controllers/CaptureViewController.swift
@@ -7,7 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
+import Cartography
+import GPUImage
 
 class CaptureViewController: UIViewController {
     
+    // MARK: - View Lifecycle
+    
+    var videoCamera: Camera?
+    var captureView: RenderView!
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = UIColor.black
+        captureView = RenderView(frame: view.bounds)
+        view.addSubview(captureView)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cameraSetup()
+        cameraCaptureViewSetup()
+    }
+    
+    // MARK: - Camera Setup
+    
+    func cameraSetup() {
+        do {
+            videoCamera = try Camera(sessionPreset: AVCaptureSessionPresetPhoto)
+        } catch {
+            print("Couldn't initialize camera, error: \(error)")
+            
+        }
+    }
+    
+    func cameraCaptureViewSetup() {
+        guard let videoCamera = videoCamera else {
+            return
+        }
+        
+        videoCamera.addTarget(captureView)
+        videoCamera.startCapture()
+    }
 }
