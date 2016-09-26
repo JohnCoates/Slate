bcb9dae6ddb6ade873803a2f6eb202d2bcdf0130
John Coates <acapulco1988@gmail.com>
2016-09-25 22:56:48 -0700

    Removed storyboard
    Manually create window, view controller

--- Source/Application/AppDelegate.swift
+++ Source/Application/AppDelegate.swift
@@ -10,12 +10,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = UIColor.white
+        window.makeKeyAndVisible()
+        window.rootViewController = CaptureViewController()
+        self.window = window
         return true
     }
 
---
+++ Source/UI/Capture/View Controllers/CaptureViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CaptureViewController.swift
+//  Slate
+//
+//  Created by John Coates on 9/25/16.
+//  Copyright © 2016 John Coates. All rights reserved.
+//
+
+import UIKit
+
+class CaptureViewController: UIViewController {
+    
+}
