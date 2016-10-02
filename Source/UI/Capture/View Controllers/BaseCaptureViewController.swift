8760ee056e755badcc61cfd964666d4eb8bcb059
John Coates <acapulco1988@gmail.com>
2016-10-01 20:13:26 -0700

    added basic draggable menu view to BaseCaptureVC

--- Source/UI/Capture/View Controllers/BaseCaptureViewController.swift
+++ Source/UI/Capture/View Controllers/BaseCaptureViewController.swift
@@ -44,8 +44,15 @@ class BaseCaptureViewController: UIViewController {
     
     // MARK: - Controls Setup
     
-    private lazy var captureButton: CaptureButton = CaptureButton()
     fileprivate func controlsSetup() {
+        captureButtonSetup()
+        controlMenuSetup()
+    }
+    
+    // MARK: - Capture Button Setup
+    
+    private lazy var captureButton: CaptureButton = CaptureButton()
+    fileprivate func captureButtonSetup() {
         captureButton.setTappedCallback(instance: self,
                                         method: Method.captureTapped)
         view.addSubview(captureButton)
@@ -57,6 +64,71 @@ class BaseCaptureViewController: UIViewController {
             $0.centerX == superview.centerX
             $0.bottom == superview.bottom - 15
         }
+    }
+    
+    // MARK: - Control Menu Setup
+    
+    fileprivate lazy var menuView = UIView()
+    fileprivate var menuVerticalConstraint: NSLayoutConstraint?
+    fileprivate func controlMenuSetup() {
+        let blurEffect = UIBlurEffect(style: .light)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        menuView.addSubview(visualEffectView)
+        constrain(visualEffectView) {
+            let superview = $0.superview!
+            $0.edges == superview.edges
+        }
+        
+        menuView.backgroundColor = UIColor.clear
+        view.addSubview(menuView)
+        
+        var verticalConstraint: NSLayoutConstraint!
+        constrain(menuView) {
+            let superview = $0.superview!
+            $0.height == 55
+            $0.width == superview.width
+            $0.top >= superview.top ~ 1000
+            $0.bottom <= superview.bottom ~ 1000
+            verticalConstraint = $0.top == superview.top + 100
+            verticalConstraint ~ 400
+        }
+        
+        menuVerticalConstraint = verticalConstraint
+        menuDraggableSetup()
+    }
+    
+    fileprivate func menuDraggableSetup() {
+        let dragGesture = UIPanGestureRecognizer(target: self,
+                                                 action: .menuDragged)
+        menuView.addGestureRecognizer(dragGesture)
+    }
+    
+    fileprivate var menuLastTranslationY: CGFloat?
+    func menuDragged(gesture: UIPanGestureRecognizer) {
+        if gesture.state == .began || gesture.state == .ended || gesture.state == .cancelled {
+            menuLastTranslationY = nil
+        }
+        guard gesture.state == .changed else {
+            return
+        }
+        
+        let translation = gesture.translation(in: menuView)
+        var translationY = translation.y
+        
+        if let menuLastTranslationY = menuLastTranslationY {
+            translationY -= menuLastTranslationY
+        }
+        
+        let minimumOffsetChange: CGFloat = 1
+        guard abs(translationY) > minimumOffsetChange,
+        let menuVerticalConstraint = menuVerticalConstraint else {
+            return
+        }
+        
+        menuLastTranslationY = translation.y
+        
+        menuVerticalConstraint.constant += translationY
+        menuView.setNeedsLayout()
     }
     
     // MARK: - Capturing
@@ -79,4 +151,10 @@ class BaseCaptureViewController: UIViewController {
 fileprivate struct Method {
     static let captureTapped = BaseCaptureViewController.captureTapped
     
+}
+
+// MARK: - Selector Extension
+
+private extension Selector {
+    static let menuDragged = #selector(BaseCaptureViewController.menuDragged)
 }
