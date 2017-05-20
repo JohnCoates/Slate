54863b3df14d21cbf106ee6360c984be3d2bae4a
John Coates <acapulco1988@gmail.com>
2017-05-20 09:40:44 -0700

    Synchronized file paths with Xcode groups
    - now uses tty-command for synx call so output shows automatically
    - updated Gemfile.lock

--- Scripts/organize.rb
+++ Scripts/organize.rb
@@ -1,8 +1,12 @@
+STDOUT.sync = true
+require 'tty-command'
+
 scriptsDirectory = __dir__
 projectDirectory = File.expand_path("..", scriptsDirectory)
 projectName = "Slate.xcodeproj"
 projectPath = File.join(projectDirectory, projectName)
 
 Dir.chdir(projectDirectory) do
-  `synx --no-sort-by-name "#{projectPath}"`
+  cmd = TTY::Command.new
+  cmd.run("synx --no-sort-by-name", projectPath)
 end
