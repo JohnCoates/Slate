2fd1d4f3dd7376ffbb232b1008b66656ead111b1
John Coates <john@johncoates.me>
2017-08-23 14:38:35 -0700

    UIView extensions
    - added ensureInHierarchy
    - added removeFRomHierarchyAsNeeded

--- Source/Extensions/UI/Views/UIView+subviews.swift
+++ Source/Extensions/UI/Views/UIView+subviews.swift
@@ -16,4 +16,30 @@ extension UIView {
         }
     }
     
+    // MARK: - Hierarchy
+    
+    func ensureInHierarchy(views: [UIView]) {
+        views.forEach(ensureInHierarchy(view:))
+    }
+    
+    func ensureInHierarchy(view: UIView) {
+        guard view.superview == nil else {
+            return
+        }
+        
+        addSubview(view)
+    }
+    
+    func removeFromHiearchyAsNeeded(views: [UIView]) {
+        views.forEach(removeFromHiearchyAsNeeded(view:))
+    }
+    
+    func removeFromHiearchyAsNeeded(view: UIView) {
+        guard view.superview != nil else {
+            return
+        }
+        
+        view.removeFromSuperview()
+    }
+    
 }
